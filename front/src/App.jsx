3b8910b7cba9cbff3f79bfd0ea4e06d39ee6c81c
dioharvandy <40176238+dioharvandy@users.noreply.github.com>
2022-04-28 18:53:50 +0700

    Routing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -12,7 +12,7 @@ import {
 import Navbar from './components/navbar';
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {RequireAuth, AlreadyLogin} from './privateRoute';
+import {RequireAuth, AlreadyLogin, PatientCounter, DoctorCounter, AdminCounter} from './privateRoute';
 
 function App() {
   return (
@@ -25,9 +25,9 @@ function App() {
                 <Route path="/register" element={<AlreadyLogin><Register/></AlreadyLogin>}></Route>
                 <Route path="/" element={<RequireAuth><Consultation/></RequireAuth>}></Route>
                 <Route path="/profile" element={<RequireAuth><Profile/></RequireAuth>}></Route>
-                <Route path="/doctor" element={<RequireAuth><Doctor/></RequireAuth>}></Route>
-                <Route path="/doctor/add" element={<RequireAuth><AddDoctor/></RequireAuth>}></Route>
-                <Route path="/consultation/add" element={<RequireAuth><AddConsultation/></RequireAuth>}></Route>
+                <Route path="/doctor" element={<RequireAuth><DoctorCounter><PatientCounter><Doctor/></PatientCounter></DoctorCounter></RequireAuth>}></Route>
+                <Route path="/doctor/add" element={<RequireAuth><DoctorCounter><PatientCounter><AddDoctor/></PatientCounter></DoctorCounter></RequireAuth>}></Route>
+                <Route path="/consultation/add" element={<RequireAuth><AdminCounter><DoctorCounter><AddConsultation/></DoctorCounter></AdminCounter></RequireAuth>}></Route>
                 <Route path="/logout" element={<RequireAuth></RequireAuth>}></Route>
                 <Route path="/consultation/edit/:id" element={<RequireAuth><EditConsultation/></RequireAuth>}></Route>
              </Routes>
--- front/src/privateRoute.jsx
+++ front/src/privateRoute.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 
-export const RequireAuth = ({ children }) => {
+const RequireAuth = ({ children }) => {
   return localStorage.getItem("token")? children : <Navigate to="/login" replace />;
 }
-export const AlreadyLogin = ({ children }) => {
+const AlreadyLogin = ({ children }) => {
 
     return localStorage.getItem("token")? <Navigate to="/" replace /> : children;
   }
+const AdminCounter = ({ children }) => {
+    return JSON.parse(localStorage.getItem("decode")).role === 1? <Navigate to="/" replace /> : children  ;
+    }
+const DoctorCounter = ({ children }) => {
+      return JSON.parse(localStorage.getItem("decode")).role === 2? <Navigate to="/" replace /> : children  ;
+      }
+const PatientCounter = ({ children }) => {
+  return JSON.parse(localStorage.getItem("decode")).role === 3? <Navigate to="/" replace /> : children  ;
+  }
+
+export {RequireAuth, AlreadyLogin, PatientCounter, AdminCounter, DoctorCounter}
